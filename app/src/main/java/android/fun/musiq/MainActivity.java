04d411d12bbd2e40b656089e6407d864a469b5cd
Samarth Mohan <samohan@akamai.com>
2015-08-09 11:46:56 -0400

    Added Spotify WebView Login

--- app/src/main/java/android/fun/musiq/MainActivity.java
+++ app/src/main/java/android/fun/musiq/MainActivity.java
@@ -1,19 +1,34 @@
 package android.fun.musiq;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final String CLIENT_ID = "55b994fa86d84e5d8847d58f1b3b1707";
+    private static final int REQUEST_CODE = 1337;
+    private static final String REDIRECT_URI = "add-that-song://callback";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
 
+        builder.setScopes(new String[]{"user-read-private"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +50,32 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            switch(response.getType()) {
+                case TOKEN:
+                    Toast loggedIn = Toast.makeText(getApplicationContext(),
+                            "You have successfully logged in", Toast.LENGTH_SHORT);
+                    loggedIn.show();
+
+                    break;
+
+                case ERROR:
+                    Toast error = Toast.makeText(getApplicationContext(),
+                            "Please verify your login credentials", Toast.LENGTH_SHORT);
+                    error.show();
+                    break;
+
+                default:
+
+            }
+        }
     }
 }
