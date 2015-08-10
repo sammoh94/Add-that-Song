06054d8b51f5a81eaf4e964856be1e0e0287cd5e
Samarth Mohan <samohan@akamai.com>
2015-08-10 14:25:39 -0400

    ACRCloud SDK implemented. Songname is displayed if it is recognized.

--- app/src/main/java/android/fun/musiq/RecognizeSong.java
+++ app/src/main/java/android/fun/musiq/RecognizeSong.java
@@ -1,23 +1,111 @@
 package android.fun.musiq;
 
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.acrcloud.rec.sdk.ACRCloudClient;
+import com.acrcloud.rec.sdk.ACRCloudConfig;
+import com.acrcloud.rec.sdk.IACRCloudListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
 
 
-public class RecognizeSong extends ActionBarActivity {
-    Button activateRecognition;
+public class RecognizeSong extends ActionBarActivity implements IACRCloudListener {
+    private ACRCloudClient mClient;
+
+    private TextView mVolume, mResult, tv_time;
+
+    private boolean mProcessing = false;
+    private boolean initState = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recognize_song);
 
-        activateRecognition = (Button) findViewById(R.id.reconSong);
+        String path = Environment.getExternalStorageDirectory().toString()
+                + "/acrcloud/model";
+
+        File file = new File(path);
+        if(!file.exists()){
+            file.mkdirs();
+        }
+
+        mVolume = (TextView) findViewById(R.id.volume);
+        mResult = (TextView) findViewById(R.id.result);
+        tv_time = (TextView) findViewById(R.id.time);
+
+        Button startBtn = (Button) findViewById(R.id.start);
+        Button cancelBtn = (Button) findViewById(R.id.cancel);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                start();
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancel();
+            }
+        });
+
     }
 
+    public void start() {
+        mVolume.setText("");
+        mResult.setText("");
+        if(!this.initState) {
+            ACRCloudConfig mConfig = new ACRCloudConfig();
+
+            mConfig.acrcloudListener = this;
+            mConfig.context = this;
+            mConfig.host = "ap-southeast-1.api.acrcloud.com";
+            mConfig.accessKey = "6a0296d0e6d98782693393599b38b6cb";
+            mConfig.accessSecret = "eSU07iIpCnL2NC9jDgCv8mjBFmVKSaaGDigzKUML";
+
+
+            mConfig.reqMode = ACRCloudConfig.ACRCloudRecMode.REC_MODE_REMOTE;
+            this.mClient = new ACRCloudClient();
+            this.initState = this.mClient.initWithConfig(mConfig);
+            if (!this.initState) {
+                Toast.makeText(this, "init error", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+        if (!mProcessing) {
+            mProcessing = true;
+            if (!this.mClient.startRecognize()) {
+                mProcessing = false;
+                mResult.setText("start error!");
+            }
+        }
+    }
+
+    protected void cancel() {
+        //if (mProcessing) {
+        this.mClient.stop();
+        tv_time.setText("");
+        mResult.setText("");
+        //}
+        mProcessing = false;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -39,5 +127,43 @@ public class RecognizeSong extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResult(String result) {
+        try {
+            JSONObject parsedResult = new JSONObject(result);
+            JSONObject status = parsedResult.getJSONObject("status");
+            JSONObject metadata = parsedResult.getJSONObject("metadata");
+            JSONArray music = metadata.getJSONArray("music");
+            JSONObject songInfo = (JSONObject) music.get(0);
+            if (status.get("msg").equals("Success")) {
+                mResult.setText(songInfo.get("title").toString());
+                mProcessing = false;
+
+                if (this.mClient != null) {
+                    this.mClient.stop();
+                    mProcessing = false;
+                }
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("MainActivity", "release");
+        if (this.mClient != null) {
+            this.mClient.release();
+            this.mClient = null;
+        }
+    }
+
+    @Override
+    public void onVolumeChanged(double volume) {
+        mVolume.setText("volume: " + volume);
     }
 }
